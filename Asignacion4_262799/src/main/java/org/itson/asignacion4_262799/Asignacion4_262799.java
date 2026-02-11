7b9dc2f58c9eb4e0cdcc1c4602947eeedb0a9a2a
AxelMonge1 <axel.monge262799@potros.itson.edu.mx>
2026-02-10 21:08:35 -0600

    Proyecto creado

---
+++ Asignacion4_262799/src/main/java/org/itson/asignacion4_262799/Asignacion4_262799.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.asignacion4_262799;
+
+/**
+ *
+ * @author axelm
+ */
+public class Asignacion4_262799 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
